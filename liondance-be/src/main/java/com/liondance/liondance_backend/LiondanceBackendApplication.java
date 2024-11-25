5a8e30cb6ef8265313594527820ca8cdbf3542e1
Viktor Kuts <vik.kuts@hotmail.com>
2024-11-25 18:36:51 -0500

    Init Backend & Docker

---
+++ liondance-be/src/main/java/com/liondance/liondance_backend/LiondanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.liondance.liondance_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiondanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiondanceBackendApplication.class, args);
+	}
+
+}
