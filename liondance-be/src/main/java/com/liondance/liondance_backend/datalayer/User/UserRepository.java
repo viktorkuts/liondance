966a963d99ecc623e7896e04644f5f08fd91a341
Futurespast <loadingrevived@gmail.com>
2024-12-02 14:08:05 -0500

    made the backend, testing next

--- liondance-be/src/main/java/com/liondance/liondance_backend/datalayer/User/UserRepository.java
+++ liondance-be/src/main/java/com/liondance/liondance_backend/datalayer/User/UserRepository.java
@@ -1,8 +1,13 @@
 package com.liondance.liondance_backend.datalayer.User;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
     Flux<User> findUsersByRolesContaining(Role role);
+    @Query("{ 'registrationStatus': { $in: ?0 }, 'roles': { $elemMatch: { $eq: 'STUDENT' } } }")
+    Flux<Student> findStudentsByRegistrationStatuses(List<RegistrationStatus> statuses);
 }
--- liondance-be/src/main/java/com/liondance/liondance_backend/logiclayer/User/UserService.java
+++ liondance-be/src/main/java/com/liondance/liondance_backend/logiclayer/User/UserService.java
@@ -1,13 +1,18 @@
 package com.liondance.liondance_backend.logiclayer.User;
 
+import com.liondance.liondance_backend.datalayer.User.RegistrationStatus;
 import com.liondance.liondance_backend.datalayer.User.Role;
 import com.liondance.liondance_backend.presentationlayer.User.StudentRequestModel;
 import com.liondance.liondance_backend.presentationlayer.User.UserResponseModel;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 public interface UserService {
     Flux<UserResponseModel> getAllUsers();
     Flux<UserResponseModel> getAllUsers(Role role);
     Mono<UserResponseModel> registerStudent(Mono<StudentRequestModel> studentRequestModel);
+    Flux<UserResponseModel> getStudentsByRegistrationStatuses(List<RegistrationStatus> statuses);
+
 }
