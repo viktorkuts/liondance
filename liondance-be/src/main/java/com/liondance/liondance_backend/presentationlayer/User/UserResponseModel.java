0d298c91297b7ee77da9a6f8c87888d49895493c
Viktor Kuts <vik.kuts@hotmail.com>
2024-12-05 13:42:38 -0500

    refac: made UserResponseModel detect which Model to return

--- liondance-be/src/main/java/com/liondance/liondance_backend/presentationlayer/User/UserResponseModel.java
+++ liondance-be/src/main/java/com/liondance/liondance_backend/presentationlayer/User/UserResponseModel.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.beans.BeanUtils;
 
 import java.time.Instant;
@@ -12,7 +13,7 @@ import java.time.LocalDate;
 import java.util.EnumSet;
 
 @Data
-@Builder
+@SuperBuilder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponseModel {
@@ -26,16 +27,14 @@ public class UserResponseModel {
     private String phone;
     private Address address;
     private EnumSet<Role> roles;
-    private Instant joinDate;
-    private RegistrationStatus registrationStatus;
-    private String parentFirstName;
-    private String parentMiddleName;
-    private String parentLastName;
-    private String parentEmail;
-    private String parentPhone;
 
     public static UserResponseModel from(User user) {
         UserResponseModel responseModel = new UserResponseModel();
+
+        if (user instanceof Student) {
+            responseModel = new StudentResponseModel();
+        }
+
         BeanUtils.copyProperties(user, responseModel);
         return responseModel;
     }
