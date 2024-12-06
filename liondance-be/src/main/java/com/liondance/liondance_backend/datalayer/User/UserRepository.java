17fe8c9de039208285ebb9fc9917858cbc6304d2
AlejandroBrnab <alejandro.rodriguez41104@gmail.com>
2024-12-06 18:56:14 -0500

    solved conflcits

--- liondance-be/src/main/java/com/liondance/liondance_backend/datalayer/User/UserRepository.java
+++ liondance-be/src/main/java/com/liondance/liondance_backend/datalayer/User/UserRepository.java
@@ -3,10 +3,13 @@ package com.liondance.liondance_backend.datalayer.User;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+
+    Mono<User> findUserByUserId (String userId);
     Flux<User> findUsersByRolesContaining(Role role);
     @Query("{ 'registrationStatus': { $in: ?0 }, 'roles': { $elemMatch: { $eq: 'STUDENT' } } }")
     Flux<Student> findStudentsByRegistrationStatuses(List<RegistrationStatus> statuses);
