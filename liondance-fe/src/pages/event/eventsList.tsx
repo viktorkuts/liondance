af3a0a3a5607be9b744d911e9336e3e7d00e6b7d
Zagreus <131713282+NotZagreus@users.noreply.github.com>
2025-01-15 09:26:12 -0500

    Feat(39) reschedule event (#43)
    * Backend done
    
    Can reschedule
    
    * Finished Frontend
    
    * BE testing done
    
    BOOM
    
    * Update DataLoaderService.java
    
    * playwright finished
    
    * Emails fixed and Fixed some previous issues
    
    * fixed test
    
    added email test

--- liondance-fe/src/pages/event/eventsList.tsx
+++ liondance-fe/src/pages/event/eventsList.tsx
@@ -96,7 +96,7 @@ function GetAllEvents() {
              {event.address?.streetAddress ?? "N/A"}
              {event.address?.city ? `, ${event.address.city}` : ""}
            </td>
-           <td>{new Date(event.eventDateTime).toLocaleString()}</td>
+               <td>{new Date(event.eventDateTime).toLocaleString("en-GB", { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' })}</td>
            <td>{event.eventType}</td>
            <td>
              {event.specialRequest && event.specialRequest.trim()
