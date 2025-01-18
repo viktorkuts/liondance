a240c295a0ae2a095c713e81f917b3b62502677c
Viktor Kuts <vik.kuts@hotmail.com>
2025-01-18 02:46:04 -0500

    fix: hacky thing..

--- liondance-fe/src/utils/axiosInstance.ts
+++ liondance-fe/src/utils/axiosInstance.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     (/^\d+$/.test(window.location.host.split(".")[0])
-      ? window.location.host.split(".")[0] + import.meta.env.BACKEND_URL
+      ? import.meta.env.BACKEND_URL.replace(
+          "://",
+          `://${window.location.host.split(".")[0]}.`
+        )
       : import.meta.env.BACKEND_URL) + "/api/v1",
   timeout: 5000,
   headers: {
