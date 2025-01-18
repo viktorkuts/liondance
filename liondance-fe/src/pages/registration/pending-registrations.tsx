c64dc4cfecf96e199b13bc63ca0406b2742363ba
Viktor Kuts <vik.kuts@hotmail.com>
2025-01-18 01:04:22 -0500

    fix: correct way, updated coolify

--- liondance-fe/src/pages/registration/pending-registrations.tsx
+++ liondance-fe/src/pages/registration/pending-registrations.tsx
@@ -13,7 +13,6 @@ function PendingRegistrations() {
   const [selectedStudent, setSelectedStudent] = useState<Student | null>(null);
 
   useEffect(() => {
-    console.log(import.meta.env.BACKEND_URL);
     const fetchPendingStudents = async () => {
       try {
         const response = await axiosInstance.get<Student[]>(
