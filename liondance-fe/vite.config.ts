1228d6252596fff401a5b6a4514fcf7fd64e46e5
Viktor Kuts <vik.kuts@hotmail.com>
2025-01-17 23:38:34 -0500

    fix: env logic

--- liondance-fe/vite.config.ts
+++ liondance-fe/vite.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
     "import.meta.env.OKTA_REDIRECT_URI": JSON.stringify(env.OKTA_REDIRECT_URI),
     "import.meta.env.OKTA_AUDIENCE": JSON.stringify(env.OKTA_AUDIENCE),
     "import.meta.env.BACKEND_URL":
-      env.COOLIFY_BRANCH && /\d+/.test(JSON.stringify(env.COOLIFY_BRANCH))
+      env.COOLIFY_BRANCH && JSON.stringify(env.COOLIFY_BRANCH).split("/").at(1)
         ? JSON.stringify(env.BACKEND_URL).replace(
             "://",
-            `://${JSON.stringify(env.COOLIFY_BRANCH).match(/\d+/)?.at(0)}`
+            `://${JSON.stringify(env.COOLIFY_BRANCH).split("/").at(1)}`
           )
         : JSON.stringify(env.BACKEND_URL),
   },
