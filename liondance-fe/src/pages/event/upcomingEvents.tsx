f687172b467d401e35d99add52384ec4520e05e5
ctsaki25 <57469766+ctsaki25@users.noreply.github.com>
2025-01-20 01:37:04 -0500

    fixed react import

--- liondance-fe/src/pages/event/upcomingEvents.tsx
+++ liondance-fe/src/pages/event/upcomingEvents.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axiosInstance from "../../utils/axiosInstance";
 import { UpcomingEvent } from "@/models/UpcomingEvent.ts";
 import "./upcomingEvents.css";
