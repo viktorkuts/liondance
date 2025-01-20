9a82138b6970a53fed60dfe1c0a65f30d587e401
Viktor Kuts <Vik.Kuts@hotmail.com>
2025-01-20 17:42:24 -0500

    fix: WORK (#62)

--- liondance-be/src/test/java/com/liondance/liondance_backend/TestSecurityConfig.java
+++ liondance-be/src/test/java/com/liondance/liondance_backend/TestSecurityConfig.java
@@ -1,22 +1,22 @@
-package com.liondance.liondance_backend;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@TestConfiguration
-@Profile("test")
-public class TestSecurityConfig {
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange(ex -> {
-                    ex.anyExchange().permitAll();
-                })
-                .csrf(ServerHttpSecurity.CsrfSpec::disable);
-        return http.build();
-    }
-}
+//package com.liondance.liondance_backend;
+//
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@TestConfiguration
+//@Profile("test")
+//public class TestSecurityConfig {
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        http
+//                .authorizeExchange(ex -> {
+//                    ex.anyExchange().permitAll();
+//                })
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+//        return http.build();
+//    }
+//}
