83f42b92d578bb3d79c07a6218e1292502c1b4b0
AlejandroBrnab <alejandro.rodriguez41104@gmail.com>
2025-01-19 20:37:09 -0500

    works

--- liondance-fe/src/pages/event/eventsList.tsx
+++ liondance-fe/src/pages/event/eventsList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axiosInstance from "../../utils/axiosInstance.ts";
 import { Event } from "@/models/Event.ts";
 import "./eventsList.css";
