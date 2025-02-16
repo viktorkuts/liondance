5a7c9ea30776a58f799f30dfc26d49f61ad40a6b
Zagreus <artemij.ca@gmail.com>
2025-02-16 13:50:48 -0500

    playwright done

--- liondance-fe/tests/subscribeToPromotions.spec.ts
+++ liondance-fe/tests/subscribeToPromotions.spec.ts
@@ -1,17 +1,15 @@
-// import { test, expect } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-// test.use({
-//     viewport: { width: 1920, height: 1080 },
-// });
-// test.use({ storageState: "playwright/.auth/client.json" });
-// test("subcribe to promotion emails", async ({ page }) => {
-//   await page.goto("/");
-//   await page.getByRole('button', { name: 'Continue', exact: true }).click();
-//   await expect(page.locator('#root')).toContainText('Promotions!');
-//   await page.getByRole('link', { name: 'Promotions!' }).click();
-//   await page.getByLabel('I want to receive promotional').check();
-//   await page.getByRole('button', { name: 'Submit' }).click();
-//   await page.getByRole('link', { name: 'Promotions!' }).click();
-//   await page.getByRole('button', { name: 'Submit' }).click();
-//   //recieved email, show class or something lol
-// });
+test.use({
+    viewport: { width: 1920, height: 1080 },
+});
+test.use({ storageState: "playwright/.auth/client.json" });
+test("subcribe to promotion emails", async ({ page }) => {
+    await page.goto("/");
+    await expect(page.locator('#root')).toContainText('Promotions!');
+    await page.getByRole('link', { name: 'Promotions!' }).click();
+    await expect(page.locator('div.m_d3ea56bb.mantine-Checkbox-labelWrapper')).toBeVisible();
+    await page.getByLabel('I want to receive promotional emails').check();
+    await page.getByRole('button', { name: 'Submit' }).click();
+    //recieved email, show class or something lol
+});
